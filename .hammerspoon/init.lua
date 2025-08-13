4e333473d6425c48f7bb2b82912ddea5876ba509
Vitaliy Krasnoperov <alistar.neron@gmail.com>
2025-08-13 11:49:53 +0200

    Update init.lua

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -2,32 +2,23 @@ local h = require("helpers")
 
 local editors = {
   { name = "Visual Studio Code", bundleIDs = { "com.microsoft.VSCode" } },
-  { name = "IntelliJ IDEA",    bundleIDs = { "com.jetbrains.intellij" } },
-  { name = "Cursor",    bundleIDs = { "com.todesktop.230313mzl4w4u92" } },
+  { name = "IntelliJ IDEA", bundleIDs = { "com.jetbrains.intellij" } },
+  { name = "Cursor", bundleIDs = { "com.todesktop.230313mzl4w4u92" } },
   -- osascript -e 'id of app "Cursor"'
 }
 
-local function getIdx() return hs.settings.get("editor_idx") or 1 end
-local function setIdx(i) hs.settings.set("editor_idx", i) end
-local function currentEditor() return editors[getIdx()] end
-
-local function showCurrent()
-  local e = currentEditor()
-  if e then hs.alert.show("Editor: " .. e.name, 0.6) end
-end
-
-hs.hotkey.bind({"alt","cmd","shift"}, "M", function()
-  local i = getIdx() + 1
+hs.hotkey.bind({"alt","cmd","shift"}, "M", function() -- Editor switching
+  local i = (hs.settings.get("editor_idx") or 1) + 1
   if i > #editors then i = 1 end
-  setIdx(i)
-  showCurrent()
+  hs.settings.set("editor_idx", i)
+  local e = editors[hs.settings.get("editor_idx") or 1]
+  if e then hs.alert.show("Editor: " .. e.name, 0.6) end
 end)
 
-hs.hotkey.bind({"alt"}, "S", function()
-  local e = currentEditor()
+hs.hotkey.bind({"alt"}, "S", function() -- Editor toggle
+  local e = editors[hs.settings.get("editor_idx") or 1]
   if e then h.toggleApp(e) end
 end)
-
 
 h.mapKey({"alt"}, "X", "Visual Studio Code", { "com.microsoft.VSCode" })
 h.mapKey({"alt"}, "P", "Bitwarden")
